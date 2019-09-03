7411f74b22a4047407aeeb0b385985756880072e
stojan-gj <stojan.gjeorgiov@gmail.com>
2019-09-03 19:28:39 +0200

    Updates

---
+++ precache-manifest.ee527da945bca09c89d7737f2587b85a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "777cdaa7d4710315d5e78b6672f0e377",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "bd3fe2e639c97600b8de",
+    "url": "/todo/static/css/main.8a48569b.chunk.css"
+  },
+  {
+    "revision": "79868bb6f35cce53fb5d",
+    "url": "/todo/static/js/2.93a86b59.chunk.js"
+  },
+  {
+    "revision": "bd3fe2e639c97600b8de",
+    "url": "/todo/static/js/main.06765fae.chunk.js"
+  },
+  {
+    "revision": "b58d1d31989374fa9307",
+    "url": "/todo/static/js/runtime~main.3db048b5.js"
+  }
+]);
